e10b98b23c4ab971eb4a9bebcbb773c4b9157c3f
aman071 <amanrehman18@gmail.com>
2020-11-14 13:16:47 +0530

    modified code with greater acc

--- traffic.py
+++ traffic.py
@@ -89,12 +89,14 @@ def get_model():
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
         tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D((2, 2)),
 
         tf.keras.layers.Flatten(),
 
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(32, activation="relu"),
-        # tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dropout(0.4),
 
         # add output layer with NUM_CATEGORIES outputs
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
